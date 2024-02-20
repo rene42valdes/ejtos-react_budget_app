50844b3163ebd281712c5ab7734cb7cdbcda5892
rene42valdes <160300816+rene42valdes@users.noreply.github.com>
2024-02-20 00:15:03 -0500

    Update Budget.js

--- src/components/Budget.js
+++ src/components/Budget.js
@@ -1,17 +1,40 @@
-import React, { useContext, useState } from 'react';
+import React, { useContext } from 'react';
 import { AppContext } from '../context/AppContext';
-
 const Budget = () => {
-    const { budget } = useContext(AppContext);
-    const [newBudget, setNewBudget] = useState(budget);
-    const handleBudgetChange = (event) => {
-        setNewBudget(event.target.value);
+    const { dispatch, expenses, budget, currency } = useContext(AppContext);
+    const totalExpenses = expenses.reduce((total, item) => {
+        return (total += item.cost);
+    }, 0);
+    const setBudget = (val) => {
+        if(val > 20000) {
+            alert("Budget exceeding  £20000");
+            return;
+        }           
+        else if(val < totalExpenses) {
+                alert("The budget must be mor than spending  £"+totalExpenses);
+                return;
+        }
+
+        dispatch({
+            type: 'SET_BUDGET',
+            payload: val
+        });
+
     }
     return (
-<div className='alert alert-secondary'>
-<span>Budget: £{budget}</span>
-<input type="number" step="10" value={newBudget} onChange={handleBudgetChange}></input>
-</div>
+        <div className='alert alert-secondary'>
+            <span>Budget: {currency}</span>
+            <input
+                        required='required'
+                        type='number'
+                        id='cost'
+                        value={budget}
+                        step="10"
+                        max="20000"
+                        style={{ marginLeft: '2rem' , size: 10}}
+                        onChange={(event) => setBudget(event.target.value)}>
+                </input>
+        </div>
     );
 };
 export default Budget;
