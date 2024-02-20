8a29af2aa1edafcd11d902b09c34e19e1e8b1c12
rene42valdes <160300816+rene42valdes@users.noreply.github.com>
2024-02-20 00:10:00 -0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -25,6 +25,9 @@ const App = () => {
                     <div className='col-sm'>
                         <ExpenseTotal />
                     </div>
+	    	    <div className='col-sm'>
+                        <CurrencyDropDown />
+                    </div>
                 </div>
                 <h3 className='mt-3'>Allocation</h3>
                 <div className='row '>
